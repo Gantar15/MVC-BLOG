a6ba8726cf0b6ce123027e6b409522e72128c713
Egor <pavlovkiy.egor@inbox.ru>
2021-04-13 12:56:23 +0300

    main edits finally annotations

--- public/scripts/annotations.js
+++ public/scripts/annotations.js
@@ -4,93 +4,116 @@
 //Так же аттрибутом data-annotation-top можно изменить положение блока с уведомлением по умолчанию, так чтобы он распологался над родительским блоком
 
 export default class Annotations{
-    annotationBlock;
-    parentNodes;
     parentNodeSelector;
 
     constructor(parentNodeSelector) {
-        this.annotationBlock = document.createElement('div');
         this.parentNodeSelector = parentNodeSelector;
-        this.parentNodes = [...document.querySelectorAll(parentNodeSelector)].filter(el => el.dataset.annotationContent);
-
-        this.addStyles();
     }
-    addStyles(){
-        this.annotationBlock.style.cssText = `
+
+    addStyles(annotationBlock){
+        annotationBlock.style.cssText = `
             background-color: rgba(72, 72, 75, 0.76);
             border-radius: 3px;
             font: normal 13px Open Sans;
             padding: 7px 8px;
             color: white;
             white-space: pre;
+            transition: all 150ms;
         `;
     }
 
-    showAnnotationsHandler = event => {
-        this.annotationBlock.style.display = "";
+    //Появление аннотации
+    fadeIn(annotationBlock, orientation){
+        if(orientation == 'top') {
+            annotationBlock.style.top = -annotationBlock.offsetHeight - 15 + 'px';
+            annotationBlock.style.opacity = "1";
+        }
+        else{
+            annotationBlock.style.bottom = -annotationBlock.offsetHeight - 15 + 'px';
+            annotationBlock.style.opacity = "1";
+        }
+    }
 
+    //Исчезновение аннотации
+    fadeOut(annotationBlock, orientation){
+        if(orientation == 'top'){
+            annotationBlock.style.transform = "translateY(-5px)";
+            annotationBlock.style.opacity = "0";
+        }
+        else{
+            annotationBlock.style.transform = "translateY(5px)";
+            annotationBlock.style.opacity = "0";
+        }
+    }
+
+    showAnnotationsHandler = event => {
         const parentNode = event.target.closest(this.parentNodeSelector);
+        if(!parentNode || !parentNode.dataset.annotationContent || parentNode.isSettled) return;
+
+        let annotationBlock = document.createElement('div');
+        this.addStyles(annotationBlock);
         parentNode.style.position = "relative";
-        this.annotationBlock.style.position = 'absolute';
-        this.annotationBlock.innerText = parentNode.dataset.annotationContent;
-        parentNode.append(this.annotationBlock);
+        annotationBlock.style.position = 'absolute';
+        annotationBlock.innerText = parentNode.dataset.annotationContent;
+        annotationBlock.style.opacity = "0";
+        parentNode.append(annotationBlock);
+        annotationBlock.style.display = "";
 
         //Если отображаем блок выше родительского блока
         let boundingRect;
         if(parentNode.hasAttribute('data-annotation-top')){
-            this.annotationBlock.style.top = -this.annotationBlock.offsetHeight - 10 + 'px';
-            boundingRect = this.annotationBlock.getBoundingClientRect();
+            this.fadeIn(annotationBlock, 'top');    //Анимируем появление аннтоции
+            annotationBlock.style.top = -annotationBlock.offsetHeight - 10 + 'px';
+            boundingRect = annotationBlock.getBoundingClientRect();
             if (boundingRect.top < 0) {                     //Если блок выше видимой области
-                this.annotationBlock.style.top = "";
-                this.annotationBlock.style.bottom = -this.annotationBlock.offsetHeight - 10 + 'px';
+                annotationBlock.style.top = "";
+                annotationBlock.style.bottom = -annotationBlock.offsetHeight - 10 + 'px';
             }
         }
         //Если отображаем блок ниже родительского блока
         else {
-            this.annotationBlock.style.bottom = -this.annotationBlock.offsetHeight - 10 + 'px';
-            boundingRect = this.annotationBlock.getBoundingClientRect();
+            this.fadeIn(annotationBlock, 'bottom');         //Анимируем появление аннтоции
+            annotationBlock.style.bottom = -annotationBlock.offsetHeight - 10 + 'px';
+            boundingRect = annotationBlock.getBoundingClientRect();
             if (boundingRect.bottom > document.documentElement.clientHeight) {      //Если блок ниже видимой области
-                this.annotationBlock.style.bottom = "";
-                this.annotationBlock.style.top = -this.annotationBlock.offsetHeight - 10 + 'px';
+                annotationBlock.style.bottom = "";
+                annotationBlock.style.top = -annotationBlock.offsetHeight - 10 + 'px';
             }
         }
 
         //Центрируем блок относительно родительского блока
-        const marg = ~~((parentNode.offsetWidth - this.annotationBlock.offsetWidth) / 2);
-        this.annotationBlock.style.left = marg + "px";
+        const marg = ~~((parentNode.offsetWidth - annotationBlock.offsetWidth) / 2);
+        annotationBlock.style.left = marg + "px";
 
-        boundingRect = this.annotationBlock.getBoundingClientRect();
+        boundingRect = annotationBlock.getBoundingClientRect();
         //Если блок левее видимой облости
         if(boundingRect.left < 0) {
-            this.annotationBlock.style.left = -boundingRect.left - Math.abs(marg) + 10 + "px";
+            annotationBlock.style.left = -boundingRect.left - Math.abs(marg) + 10 + "px";
         }
         //Если блок правее видимой облости
         else if(boundingRect.right > document.documentElement.scrollWidth) {
             let diff = boundingRect.right - document.documentElement.clientWidth;
-            this.annotationBlock.style.left = "";
-            this.annotationBlock.style.right = diff - Math.abs(marg) + 10 + "px";
+            annotationBlock.style.left = "";
+            annotationBlock.style.right = diff - Math.abs(marg) + 10 + "px";
+        }
+
+        parentNode.isSettled = true;            //Ключ для того, чтобы координаиты расситывались только один раз
+        parentNode.onpointerleave = () => {
+            parentNode.isSettled = false;
+            this.hideAnnotationsHandler(annotationBlock, parentNode.hasAttribute('data-annotation-top') ? 'top' : 'bottom');
         }
     }
-    hideAnnotationsHandler = () => {
-        this.annotationBlock.style.display = "none";
-        this.annotationBlock.style.bottom = "";
-        this.annotationBlock.style.top = "";
-        this.annotationBlock.style.left = "";
-        this.annotationBlock.style.right = "";
+
+    hideAnnotationsHandler = (annotationBlock, orientation) => {
+        this.fadeOut(annotationBlock, orientation);
+        setTimeout(() => annotationBlock.remove(), 155)
     }
+
     dispatch(){
-        this.parentNodes.forEach(el => {
-            if(!el.annotationChecked) {
-                el.addEventListener('mouseenter', this.showAnnotationsHandler);
-                el.addEventListener('mouseleave', this.hideAnnotationsHandler);
-                el.annotationChecked = true;
-            }
-        });
+        document.addEventListener('pointerover', this.showAnnotationsHandler);
     }
+
     turnOff(){
-        parentNodes.forEach(el => {
-            el.removeEventListener('mouseenter', this.showAnnotationsHandler);
-            el.removeEventListener('mouseleave', this.hideAnnotationsHandler);
-        });
+        document.removeEventListener('pointerover', this.showAnnotationsHandler);
     }
 }
